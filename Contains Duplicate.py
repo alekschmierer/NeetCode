ef06246f20019215df3b416144707d641b4571a8
alek <alek.schmierer2022@gmail.com>
2025-07-12 15:23:41 -0700

    NeetCode

---
+++ Contains Duplicate.py
@@ -0,0 +1,29 @@
+#Understand
+#Detect if list contains duplciates
+
+#Match
+#Set question
+
+#Plan/Implement
+from typing import List
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        mySet = set(nums)
+        if len(mySet) != len(nums):
+            return True
+        else:
+            return False
+
+#Review
+    #Example 1:
+        # Input: nums = [1, 2, 3, 3]
+        # Output: true
+    # Example 2:
+        # Input: nums = [1, 2, 3, 4]
+        # Output: false
+
+#Evaluate
+    #Time Complexity:
+    #Space Complexity:
+
+#Completed 7/12/25
